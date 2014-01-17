2870488c0ae460b1ac988b2ba6618ddfaeb7d11a
rileynat <rileynat@umich.edu>
2014-01-17 17:03:18 -0500

    initial commit adding in hex string support and extra colors

---
+++ UIColor+HSSExtraColors.h
@@ -0,0 +1,25 @@
+//
+//  UIColor+HSSExtraColors.h
+//  BusinessCardMockup
+//
+//  Created by Nathan Riley on 1/17/14.
+//  Copyright (c) 2014 Nathan Riley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HSSExtraColors)
+
++ (UIColor*)lightBlueColor;
+
++ (UIColor*)pinkColor;
+
++ (UIColor*)lightGreenColor;
+
++ (UIColor*)maroonColor;
+
++ (UIColor*)michiganMaizeColor;
+
++ (UIColor*)michiganBlueColor;
+
+@end
---
+++ UIColor+HSSExtraColors.m
@@ -0,0 +1,37 @@
+//
+//  UIColor+HSSExtraColors.m
+//  BusinessCardMockup
+//
+//  Created by Nathan Riley on 1/17/14.
+//  Copyright (c) 2014 Nathan Riley. All rights reserved.
+//
+
+#import "UIColor+HSSExtraColors.h"
+
+@implementation UIColor (HSSExtraColors)
+
++ (UIColor*)lightBlueColor {
+    return [UIColor colorWithRed:0.60 green:0.87 blue:0.98 alpha:1.0];
+}
+
++ (UIColor*)pinkColor {
+    return [UIColor colorWithRed:1 green:0.45 blue:0.75 alpha:1];
+}
+
++ (UIColor*)lightGreenColor {
+    return [UIColor colorWithRed:0.56 green:0.93 blue:0.56 alpha:1];
+}
+
++ (UIColor*)maroonColor {
+    return [UIColor colorWithRed:0.48 green:0.07 blue:0.075 alpha:1];
+}
+
++ (UIColor*)michiganMaizeColor {
+    return [UIColor colorWithRed:1 green:0.8 blue:0.02 alpha:1];
+}
+
++ (UIColor*)michiganBlueColor {
+    return [UIColor colorWithRed:0 green:0.15 blue:0.3 alpha:1];
+}
+
+@end
